f7f732c2c64cef749fe5e79e4e8e59726e6c2051
Hasan Aboul Hasan <hassan.cs91@gmail.com>
2024-03-11 11:51:21 +0200

    added endpoint 3

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Python FastAPI Is Running..."}
 
-
 @app.get("/test/call")
 def read_root():
     return {"Hello": "this is a test endpoint"}
@@ -15,3 +14,8 @@ def read_root():
 @app.get("/test/call2")
 def read_root():
     return {"Hello": "this is a test endpoint 2"}
+
+
+@app.get("/test/call3")
+def read_root():
+    return {"Hello": "this is a test endpoint 3"}
